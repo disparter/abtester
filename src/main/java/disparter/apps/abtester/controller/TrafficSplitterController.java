f6a00c3fc511619ff525e9ca34e43805262da882
Felipe Faria <felipe.a.b.faria@gmail.com>
2022-03-15 17:10:54 -0300

    Adding shell TrafficSplitterController

---
+++ src/main/java/disparter/apps/abtester/controller/TrafficSplitterController.java
@@ -0,0 +1,12 @@
+package disparter.apps.abtester.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("abtest")
+public class TrafficSplitterController {
+
+    public void split(){
+
+    }
+
+}
---
+++ src/test/java/disparter/apps/abtester/controller/AbtesterApplicationTests.java
@@ -0,0 +1,11 @@
+package disparter.apps.abtester.controller;
+
+import org.junit.jupiter.api.Test;
+
+class TrafficSplitterControllerTests {
+
+	@Test
+	void test_splitOn() {
+	}
+
+}
